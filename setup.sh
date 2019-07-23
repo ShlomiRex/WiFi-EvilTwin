f81900aa5a2ccab594475afaaddc527825bda6a6
root <root@localhost.localdomain>
2019-07-23 14:06:05 +0000

    Commit before loosing my mind, can't spoof dns entry

--- setup.sh
+++ setup.sh
@@ -1,3 +1,5 @@
-sudo apt-get update && sudo apt-get install hostapd dnsmasq
+sudo apt-get update && sudo apt-get install hostapd dnsmasq apache2 -y
 mkdir /var/log/WiFi-EvilTwin/
 touch /var/log/WiFi-EvilTwin/dnsmasq.log
+echo "Starting apache2 web server at localhost:80"
+service apache2 start
--- tools/start-dnsmasq.sh
+++ tools/start-dnsmasq.sh
@@ -1,2 +1,4 @@
+echo "Killing all dnsmasq processes..."
 killall dnsmasq
+echo "Starting dnsmasq..."
 dnsmasq -C conf/dnsmasq.conf
