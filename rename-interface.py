fdaed8c78cf612d1dbba395b3412a1476a8099d6
Shlomi Laptop Ubuntu <shlomid@protonmail.com>
2019-08-14 18:31:22 +0300

    Added rename-interface.py script to change name of wireless interface

---
+++ rename-interface.py
@@ -0,0 +1,9 @@
+import os
+print("Interfaces:")
+os.system('sudo iwconfig')
+old_inter = raw_input("Type in old interface name:")
+new_inter = raw_input("Type in new interface name:")
+os.system("sudo ifconfig "+old_inter+" down")
+os.system("sudo ip link set "+old_inter+" name "+new_inter)
+os.system("sudo ifconfig "+new_inter+" up")
+print("Successfuly changed interface \'"+old_inter+"\' to \'"+new_inter)
